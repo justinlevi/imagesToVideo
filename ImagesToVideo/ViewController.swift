3a438f3350c06c4a7c5d5b4ab38d16e41d4738f5
Justin Winter <justinlevi@gmail.com>
2015-09-15 14:14:45 -0400

    some additional optimizations, progress view, render time test

--- ImagesToVideo/ViewController.swift
+++ ImagesToVideo/ViewController.swift
@@ -10,18 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var progressView: UIProgressView!
+  @IBOutlet weak var progressLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     let path = NSBundle.mainBundle().pathForResource("hasselblad-01", ofType: "jpg")!
     
     var photosArray = [String]()
-    for _ in 0...500 {
+    for _ in 0...5000 {
       photosArray.append(path)
     }
     
     let tlb = TimeLapseBuilder(photoURLs: photosArray)
     tlb.build({ (progress) -> Void in
+      
+      dispatch_async(dispatch_get_main_queue()){
+        self.progressLabel.text = "rendering \(progress.completedUnitCount) of \(progress.totalUnitCount) frames"
+        self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
+      }
       
       }, success: { (url) -> Void in
         print("SUCCESS: \(url)")
